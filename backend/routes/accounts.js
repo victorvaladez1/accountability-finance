7fac10bb05ea15ab31f3031cd1db15ee9dbe6cb1
Victor Valadez <vvaladez315@gmail.com>
2025-04-10 12:55:18 -0500

    Added update and delete routes for accounts

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -32,4 +32,43 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
+// Update account
+router.put('/:id', verifyToken, async (req, res) => {
+    const { name, type, balance } = req.body;
+
+    try {
+        //Find account owned by the logged-in user
+        
+        const account = await Account.findOne({ _id: req.params.id, user: req.user.id });
+        
+        if (!account) return res.status(404).json({message: "Account not found"});
+
+        // Update fields if provided
+        if (name) account.name = name;
+        if (type) account.type = type;
+        if (balance !== undefined) account.balance = balance;
+
+        const updated = await account.save();
+        res.json(updated);
+    } catch (err) {
+        res.status(500).json({ message: "Server error", error: err.message });
+    }
+});
+
+// Delete account
+router.delete("/:id", verifyToken, async (req, res) => {
+    try {
+        const account = await Account.findOneAndDelete({
+            _id: req.params.id,
+            user: req.user.id,
+        });
+
+        if (!account) return res.status(404).json({ message: "Account not found" });
+
+        res.json({ message: "Account deleted successfully "});
+    } catch (err) {
+        res.status(500).json({ messsage: "Server error", error: err.message });
+    }
+});
+
 module.exports = router;
