16c2198ab88c6b129b9117c66aed2340b57ad395
Victor Valadez <vvaladez315@gmail.com>
2025-05-13 11:02:00 -0500

    set axios withCredentials to support cross-origin login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from "axios";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
@@ -9,15 +10,17 @@ import Planning from "./pages/Planning";
 import ChatCoach from "./pages/ChatCoach";
 import Portfolio from "./pages/Portfolio";
 
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.withCredentials = true;
+
 function App() {
   return (
     <Router>
       <Routes>
-
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/planning" element={<Planning />} />
-        <Route path="/chat-coach" element={<ChatCoach />}/>
+        <Route path="/chat-coach" element={<ChatCoach />} />
         <Route path="/portfolio" element={<Portfolio />} />
         
         <Route 
